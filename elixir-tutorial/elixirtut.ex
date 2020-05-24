1295700335ab8015c90db2a8039610db3b95aa37
André Ferreira <contato@andreferreira.me>
2020-05-24 15:12:17 -0300

    Add do_stuff

--- elixir-tutorial/elixirtut.ex
+++ elixir-tutorial/elixirtut.ex
@@ -1,6 +1,7 @@
 defmodule M do
     def main do
         # data_stuff()
+        do_stuff()
     end
 
     def data_stuff do
@@ -32,4 +33,123 @@ defmodule M do
         # Pipe
         4 * 10 |> IO.puts
     end
+
+    def do_stuff do
+        IO.puts "5 + 4 = #{5+4}"
+        IO.puts "5 - 4 = #{5-4}"
+        IO.puts "5 * 4 = #{5*4}"
+        IO.puts "5 / 4 = #{5/4}"
+        IO.puts "5 div 4 = #{div 5, 4}"
+        IO.puts "5 rem 4 = #{rem 5, 4}"
+
+        IO.puts "4 == 4.0 : #{4 == 4.0}"
+        IO.puts "4 === 4.0 : #{4 === 4.0}"
+        IO.puts "4 != 4.0 : #{4 != 4.0}"
+        IO.puts "4 !== 4.0 : #{4 !== 4.0}"
+
+        IO.puts "5 > 4 : #{5 > 4}"
+        IO.puts "5 >= 4 : #{5 >= 4}"
+        IO.puts "5 < 4 : #{5 < 4}"
+        IO.puts "5 <= 4 : #{5 <= 4}"
+
+        age = 16
+
+        IO.puts "Vote and drive : #{age >= 16 and age >= 18}"
+        IO.puts "Vote or drive : #{age >= 16 or age >= 18}"
+
+        IO.puts not true
+
+        if age >= 18 do
+            IO.puts "Can vote"
+        else
+            IO.puts "Can't vote"
+        end
+
+        unless age === 18 do
+            IO.puts "You're not 18"
+        else
+            IO.puts "You are 18"
+        end
+
+        cond do
+            age >= 18 -> IO.puts "You can Vote"
+            age >= 16 -> IO.puts "You can drive"
+            age >= 14 -> IO.puts "You can wait"
+            true -> IO.puts "Default"
+        end
+
+        case 2 do
+            1 -> IO.puts "Entered 1"
+            2 -> IO.puts "Entered 2"
+            _ -> IO.puts "Default"
+        end
+
+        IO.puts "Ternary : #{if age > 18, do: "Can vote", else: "Can't vote"}"
+
+        my_stats = {175, 6.25, :Derek}
+
+        IO.puts "Tuple #{is_tuple my_stats}"
+
+        my_stats2 = Tuple.append(my_stats, 42)
+
+        IO.puts "Age #{elem(my_stats2, 3)}"
+
+        IO.puts "Size #{tuple_size(my_stats2)}"
+
+        my_stats3 = Tuple.delete_at my_stats2, 0
+        my_stats4 = Tuple.insert_at my_stats3, 0, 1974
+
+        many_zeroes = Tuple.duplicate 0, 5
+
+        {weight, height, name} = {175, 6.25, "Derek"}
+
+        IO.puts "Weigth: #{weight}"
+        IO.puts "Heigth: #{height}"
+        IO.puts "Name: #{name}"
+
+        list1 = [1, 2, 3]
+        list2 = [4, 5, 6]
+
+        list3 = list1 ++ list2
+        list4 = list3 -- list1
+
+        IO.puts 6 in list4
+
+        [head | tail] = list3
+
+        IO.inspect list3
+
+        IO.puts "Head : #{head}"
+        IO.write "Tail : "
+        IO.inspect tail
+
+        IO.inspect [97,98], charlists: :as_lists
+
+        Enum.each tail, fn item ->
+            IO.puts item
+        end
+
+        words = ["Random", "Words", "in a", "list"]
+        Enum.each words, fn word ->
+            IO.puts word
+        end
+
+        display_list(words)
+
+        IO.puts display_list(List.delete(words, "Random"))
+        IO.puts display_list(List.delete_at(words, 1))
+
+        IO.puts List.first words
+
+        IO.puts List.last words
+
+        my_stats = [name: "Derek", height: 6.25]
+    end
+    
+    def display_list([word|words]) do
+        IO.puts word
+        display_list(words)
+    end
+
+    def display_list([]), do: nil
 end
