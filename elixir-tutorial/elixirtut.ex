caf4ef207b4707f9eb42769e009242fc6ad33a8f
André Ferreira <contato@andreferreira.me>
2020-05-24 21:26:59 -0300

    Add anonymous functions

--- elixir-tutorial/elixirtut.ex
+++ elixir-tutorial/elixirtut.ex
@@ -165,6 +165,24 @@ defmodule M do
 
         [_, [_, a]] = [20, [30, 40]]
         IO.puts "Get num: : #{a}"
+
+        get_sum = fn x, y -> x + y end
+
+        IO.puts "5 + 5 = #{get_sum.(5, 5)}"
+
+        get_less = &(&1 - &2)
+
+        IO.puts "7 - 6 = #{get_less.(7, 6)}"
+
+        add_sum = fn
+            {x, y} -> IO.puts "#{x} + #{y} = #{x+y}"
+            {x, y, z} -> IO.puts "#{x} + #{y} + #{z} = #{x+y+z}"
+        end
+
+        add_sum.({1, 2})
+        add_sum.({1, 2, 3})
+
+        IO.puts do_it()
     end
 
     def display_list([word|words]) do
@@ -173,4 +191,9 @@ defmodule M do
     end
 
     def display_list([]), do: nil
+
+    # Default values
+    def do_it(x \\ 1, y \\ 1) do
+        x + y
+    end
 end
